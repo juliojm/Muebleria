aa6bc978c4c80f6bc111f274fe04d8dc6e4845b0
juliojm <theyulius_2703@hotmail.com>
2014-05-07 07:38:07 -0500

    Base de datos

---
+++ app/basedatos.sql
@@ -0,0 +1,164 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.6.10 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             8.3.0.4694
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Volcando estructura de base de datos para muebleria
+CREATE DATABASE IF NOT EXISTS `muebleria` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `muebleria`;
+
+
+-- Volcando estructura para tabla muebleria.archivos
+CREATE TABLE IF NOT EXISTS `archivos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_archivo` varchar(255) DEFAULT NULL,
+  `modelo_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.archivos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `archivos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `archivos` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.compras
+CREATE TABLE IF NOT EXISTS `compras` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `piezas` decimal(10,0) DEFAULT NULL,
+  `monto_compra` decimal(10,0) DEFAULT NULL,
+  `material_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.compras: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `compras` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compras` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.consumibles
+CREATE TABLE IF NOT EXISTS `consumibles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `material_id` int(11) DEFAULT NULL,
+  `modelo_id` int(11) DEFAULT NULL,
+  `cantidad` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.consumibles: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `consumibles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `consumibles` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.materials
+CREATE TABLE IF NOT EXISTS `materials` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `costo` decimal(10,0) DEFAULT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.materials: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.materials_suppliers
+CREATE TABLE IF NOT EXISTS `materials_suppliers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `material_id` int(11) DEFAULT NULL,
+  `suppliers_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.materials_suppliers: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `materials_suppliers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `materials_suppliers` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.modelos
+CREATE TABLE IF NOT EXISTS `modelos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `cantidad_en_bodega` decimal(10,0) DEFAULT NULL,
+  `cantidad_en_fabrica` decimal(10,0) DEFAULT NULL,
+  `precio` decimal(10,0) DEFAULT NULL,
+  `costo` decimal(10,0) DEFAULT NULL,
+  `tipo_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.modelos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `modelos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `modelos` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.suppliers
+CREATE TABLE IF NOT EXISTS `suppliers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `telefono` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `contacto` varchar(255) DEFAULT NULL,
+  `material_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.suppliers: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.tipos
+CREATE TABLE IF NOT EXISTS `tipos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.tipos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `tipos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipos` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.users: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla muebleria.ventas
+CREATE TABLE IF NOT EXISTS `ventas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `modelo_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla muebleria.ventas: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
