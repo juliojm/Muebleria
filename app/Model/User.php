0467d4cc04a433086baacc7825b475d039d42cc2
juliojm <theyulius_2703@hotmail.com>
2014-05-09 08:36:44 -0500

    beforeSave del modelo User

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,6 +6,18 @@ App::uses('AppModel', 'Model');
  */
 class User extends AppModel {
 
+/************************************************************/
+
+public function beforeSave($options = array()) {
+	if (isset($this->data[$this->alias]['password'])) {
+		$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+	}
+	return true;
+}	
+
+/************************************************************/
+
+
 /**
  * Display field
  *
