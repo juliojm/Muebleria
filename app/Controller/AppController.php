c4c35197583b22dd1f552a50ae65fe33bc1dcc35
juliojm <theyulius_2703@hotmail.com>
2014-05-12 08:59:53 -0500

    Solucionado conflicto

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -3,20 +3,9 @@
 App::uses('Controller', 'Controller');
 
 class AppController extends Controller {
-<<<<<<< HEAD
 
 	public $components	=	array('Auth','Session');
 	public function beforeFilter(){
 			
 	}
-	
-	
-=======
-	public $scaffold;
-	public $components=array('Auth','Session');
-	public function beforeFilter(){
-		$this->Auth->allow('index','view','add','edit','delet');
-	
-	}
->>>>>>> 39a25622de94c0b4193a12b14ebf3cfe1d656352
 }
