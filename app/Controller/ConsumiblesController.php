25581ae0e1db17021be90ab88e3dd08cd12ff4f5
juliojm <theyulius_2703@hotmail.com>
2014-06-09 08:36:28 -0500

    Controladores de consumibles

--- app/Controller/ConsumiblesController.php
+++ app/Controller/ConsumiblesController.php
@@ -2,15 +2,93 @@
 App::uses('AppController', 'Controller');
 
 class ConsumiblesController extends AppController {
+				 /* CONTROLODADOR ELABORADO POR  --JULIO--*/
 
-/************************************************************/
+/*****************************************************************************************************/
 
 	public function beforeFilter(){
 		parent::beforeFilter();
 		
 	}
 
-/************************************************************/
+/*****************************************************************************************************/
+	public function index(){
+		$data = $this->Consumible->find("all");	
+		$this->set('data' , $data);
+		
+	}
+	
+/*****************************************************************************************************/
+	
+	public function registrar(){
+		
+		$lstMateriales=$this->Consumible->Material->find('list');
+		$this->set('lstMateriales',$lstMateriales);
+		
+		$lstModelos=$this->Consumible->Modelo->find('list');
+		$this->set('lstModelos',$lstModelos);
+		
+		if ($this->request->is('post')) {
+			$this->Consumible->create();
+			if ($this->Consumible->save($this->request->data)) {
+				$this->Session->setFlash('Este Consumible se ha registrado con éxito');
+				$this->redirect('/cosumibles/index');
+			}
+			$this->Session->setFlash('Ocurrió un problema al momento de registrar el Consumible.');
+		}
+	
+	}
+	
+/*****************************************************************************************************/	
+	public function ver($id){	
 
+	if($data=$this->Consumible->findById($id)){
+			$this->set('data',$data);
+		}
+		else{
+			$this->Session->setFlash('No se encontro el ID de este Consumible');
+			$this->redirect('/consumibles/');
+		}
+		
+}	
 
+/*****************************************************************************************************/
+	public function borrar($id){
+	
+		if($this->Consumible->delete($id)){
+			$this->Session->setFlash('Este Consumible se borro correctamente');
+			$this->redirect('/consumibles/index');
+		}
+		$this->Session->setFlash('Ocurrio un problema al borrar el Consumible');
+		$this->redirect('/consumibles/index');
+		
+		
 }
+
+/*****************************************************************************************************/
+	public function editar($id=-9999999){
+	
+		if ($id==-9999999){
+			$this->Session->setFlash('Necesitas especificar un Consumible');
+			$this->redirect('/consumibles/index');
+		}
+		if ($this->request->is('post')) {
+			$this->Consumible->id=$id;
+			if($this->Consumible->save($this->request->data)){
+				$this->Session->setFlash('El regsitro se ha modificado exitosamente');
+				$this->redirect('/consumibles/');
+			}
+			$this->Session->setFlash('Ocurrio un problema al modificar el registro');
+		}
+		if($data=$this->Consumible->findById($id)){
+			$this->set('data',$data);
+		}
+		else{
+			$this->Session->setFlash('No se encontro a este Consumible');
+			$this->redirect('/consumibles/');
+		}
+			
+	}
+}
+	
+/***************************************************************************************************/
---
+++ app/View/Consumibles/registrar.ctp
@@ -0,0 +1,7 @@
+﻿<?php
+	echo"<h2>Aquí registras tu Consumible.</h2>";
+	echo $this->Form->create();
+	echo $this->Form->input('material_id',array('options'=>$lstMateriales));
+	echo $this->Form->input('modelo_id',array('options'=>$lstModelos));
+	echo $this->Form->input('cantidad');
+	echo $this->Form->end('Registar');
