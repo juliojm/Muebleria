670ad958031276b95184a994eed58194fbb83d88
juliojm <theyulius_2703@hotmail.com>
2014-06-19 19:06:05 -0500

    Herramientas para crear pdf´s

--- app/Controller/ArchivosController.php
+++ app/Controller/ArchivosController.php
@@ -3,6 +3,8 @@ App::uses('AppController', 'Controller');
 
 class ArchivosController extends AppController {
 
+
+
 /************************************************************/
 
 	public function beforeFilter(){
@@ -11,6 +13,24 @@ class ArchivosController extends AppController {
 	}
 
 /************************************************************/
+						/*Por Julio*/
+public function descargar($id = null)
+    {
+        if (!$id)
+        {
+            $this->Session->setFlash('No has seleccionado ningun PDF');
+            $this->redirect('/archivos/index');
+        }else{
+        // Sobrescribimos para que no aparezcan los resultados de debuggin
+        // ya que sino daria un error al generar el pdf.
+        Configure::write('debug',0);
+        $resultado = $this->Archivo->findById($id); 
+        $this->set("datos_pdf",$resultado);
+        $this->layout = 'pdf'; //esto usara el layout pdf.ctp
+        $this->render();
+    }}
+/************************************************************/
+public function index(){
 
-
+	}
 }
---
+++ app/View/Archivos/descargar.ctp
@@ -0,0 +1,31 @@
+﻿
+<?php
+
+		// Vista Por Julio
+App::import('Vendor','xtcpdf'); 
+$tcpdf = new XTCPDF();
+$textfont = 'freesans';
+
+$tcpdf->SetAuthor("");
+$tcpdf->SetAutoPageBreak( false );
+$tcpdf->setHeaderFont(array($textfont,'',10));
+$tcpdf->xheadercolor = array(255,255,255);
+$tcpdf->xheadertext = 'Fecha: '. date('d-m-Y',time());
+$tcpdf->xfootertext = 'www.pedroventura.com';
+
+// Ahora imprimimos el contenido de la pagina en una posición determinada
+// estos datos son un ejemplo, y en mi ejemplo hay un pequeño texto y una imagen.
+$tcpdf->AddPage();
+$tcpdf->SetTextColor(0, 0, 0);
+$tcpdf->SetFont($textfont,'B',10);
+$tcpdf->Cell(10,20,'Nombre:', 0, 0);
+// configuramos la calidad de JPEG
+$tcpdf->setJPEGQuality(100);
+$tcpdf->Image($imagen, 0, 50, 200, 200, '', '', '', false, 150);  
+
+// se pueden asignar mas datos, ver la documentación de TCPDF
+
+echo $tcpdf->Output('mi_archivo.pdf', 'D'); //el pdf se muestra en el navegador
+//echo $tcpdf->Output('mi_archivo.pdf', 'I'); //el pdf se descarga
+
+?>
