c5898b16bf74640f433ad90bc3c98f9072b7c9e0
osvil90 <osvaldo106_@hotmail.com>
2014-06-22 11:32:25 -0500

    comentarios

--- app/Controller/ModelosController.php
+++ app/Controller/ModelosController.php
@@ -97,32 +97,7 @@ public function index(){
 
 /*****************************************************************************************************/
 
-/*public function archivos() {
-         
-        $this->Upload->create();
-         
-           if ($this->request->is('post')) {
-                
-            if( $this->data['Upload']['archivo']['error'] == 0 &&  $this->data['Upload']['archivo']['size'] > 0)
-             {
-                // Informacion del tipo de archivo subido $this->data['Upload']['archivo']['type']
-                  $destino = WWW_ROOT.'uploads'.DS;
-                  if(move_uploaded_file($archivo['tmp_name'], $destino.$archivo['name']))
-                   {               
-                      $this->Session->setFlash(__('El archivo se a guardado'));
-                   }
-                  else
-                   {
-                          $this->Session->setFlash(__('El archivo no se pudo subir, por favor intentelo de nuevo'));       
-                   }
-                   $this->redirect(array('action' => 'index'));
-             }else{
-                  $this->Session->setFlash(__('Error al intentar subir el archivo'));
-              }
-        }
-         
-    }
-	*/
+
 
 
 
